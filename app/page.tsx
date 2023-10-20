16ee557caf2c1398ff2788e83846de3339189184
Ismael Bautista <ismaelbautista6@gmail.com>
2023-10-20 17:30:27 +0200

    improved mobile version h1

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='container mx-auto'>
-        <p className='md:text-5xl lg:text-6xl py-4 text-center'>Need a GM?</p>      
+        <h1 className='text-5xl lg:text-6xl py-4 text-center'>Need a GM?</h1>      
         <div className='flex justify-center items-center gap-8 flex-col'>
           <GMFetcher />
           <CountGm />
